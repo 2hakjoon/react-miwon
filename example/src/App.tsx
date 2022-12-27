e63973ff8e1c509f992d053cf788c5297ee895c8
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-27 23:04:00 +0900

    INSTALL - react-miwon new version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,12 @@
 import { addMiwon } from 'miwon'
-import { MiwonProvider } from 'react-miwon'
+import { MiwonAsyncBoundary, MiwonProvider } from 'react-miwon'
 
 function App() {
   const store = addMiwon({})
   console.log(MiwonProvider)
   return (
     <>
+      <MiwonAsyncBoundary />
       <MiwonProvider store={store}>리액트 앱</MiwonProvider>
     </>
   )
