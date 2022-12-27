2bb1811b3282b6656cf3e5b9376b321bf19e89b4
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-27 23:03:27 +0900

    ADD - export miwon-boundaries

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './components/MiwonProvider'
 export * from './components/MiwonContext'
+export * from 'miwon-boundaries'
 
 // export { MiwonContext } from './types/compnents/MiwonContxt.types'
 // export { MiwonProvider } from './types/compnents/MiwonProvider.type'
