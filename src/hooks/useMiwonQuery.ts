49352455ee36878c769c31db54e86cdcd0f2cb01
HAKJOON0223 <2hakjoon@gmail.com>
2023-01-07 00:20:29 +0900

    IMPLEMENT - partially useMiwonQuery

--- src/hooks/useMiwonQuery.ts
+++ src/hooks/useMiwonQuery.ts
@@ -1,1 +1,28 @@
-export const useMiwonQuery = () => {}
+import { useState, useSyncExternalStore } from 'react'
+import { useMiwonStore } from './useMiwonStore'
+
+export const useMiwonQuery = <T, V>(
+  url: string,
+  normalizer: (res: any) => any
+) => {
+  const { getState, subscribe, miwonQuery } = useMiwonStore()
+  const state = useSyncExternalStore(subscribe, getState)
+
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const fetch = () => {
+    setLoading(true)
+    miwonQuery(url, normalizer)
+      .then((res: any) => {
+        setData(res)
+        setLoading(false)
+      })
+      .catch((err: any) => {
+        setError(err)
+      })
+  }
+  fetch()
+  return { data, loading, error }
+}
