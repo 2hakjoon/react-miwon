8c01894f35d5474ef47fd38c58ecfd4304fb910c
HAKJOON0223 <2hakjoon@gmail.com>
2023-01-24 22:41:56 +0900

    ADD - getServerSnapshot

--- src/hooks/useMiwonSelector.ts
+++ src/hooks/useMiwonSelector.ts
@@ -3,5 +3,9 @@ import { useMiwonStore } from './useMiwonStore'
 
 export const useMiwonSelector = <T>(selector: (state: any) => T): T => {
   const { subscribe, getState } = useMiwonStore()
-  return useSyncExternalStore(subscribe, () => selector(getState()) as any) as T
+  return useSyncExternalStore(
+    subscribe,
+    () => selector(getState()) as any,
+    () => true
+  ) as T
 }
