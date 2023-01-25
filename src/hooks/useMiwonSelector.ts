d533410dbb8b1ab2f4ac17c77b5be650e78696e2
HAKJOON0223 <2hakjoon@gmail.com>
2023-01-25 22:36:07 +0900

    CHANGE - useMiwonSelector ssr data

--- src/hooks/useMiwonSelector.ts
+++ src/hooks/useMiwonSelector.ts
@@ -1,11 +1,14 @@
 import { useSyncExternalStore } from 'react'
+import { isServerSide } from '../components/utils/runtime'
 import { useMiwonStore } from './useMiwonStore'
 
-export const useMiwonSelector = <T>(
-  selector: (state: any) => T,
-  getServerSideSnapshot: () => boolean = () => false
-): T => {
+export const useMiwonSelector = <T>(selector: (state: any) => T): T => {
   const { subscribe, getState } = useMiwonStore()
+
+  const getServerSideSnapshot = () => {
+    return isServerSide() && selector(getState())
+  }
+
   return useSyncExternalStore(
     subscribe,
     () => selector(getState()) as any,
